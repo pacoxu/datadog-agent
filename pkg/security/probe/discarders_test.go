7f0df6289d5df8d60ffdb0f1a1ce4b7e692d7480
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2022-09-28 16:57:15 +0200

    [CWS] kernel side discarder flush

--- pkg/security/probe/discarders_test.go
+++ pkg/security/probe/discarders_test.go
@@ -37,7 +37,7 @@ func addRuleExpr(t testing.TB, rs *rules.RuleSet, exprs ...string) {
 }
 
 func TestIsParentDiscarder(t *testing.T) {
-	id := newInodeDiscarders(nil, nil, nil)
+	id := newInodeDiscarders(nil, nil)
 
 	enabled := map[eval.EventType]bool{"*": true}
 
@@ -220,7 +220,7 @@ func TestIsParentDiscarder(t *testing.T) {
 }
 
 func TestIsGrandParentDiscarder(t *testing.T) {
-	id := newInodeDiscarders(nil, nil, nil)
+	id := newInodeDiscarders(nil, nil)
 
 	enabled := map[eval.EventType]bool{"*": true}
 
@@ -395,7 +395,7 @@ func TestIsDiscarderOverride(t *testing.T) {
 }
 
 func BenchmarkParentDiscarder(b *testing.B) {
-	id := newInodeDiscarders(nil, nil, nil)
+	id := newInodeDiscarders(nil, nil)
 
 	enabled := map[eval.EventType]bool{"*": true}
 
